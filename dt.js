d99e9281ef97a32dea6bc5608d08f4f4df3fd74c
Gjorka <113481480+Gjorka@users.noreply.github.com>
2022-09-18 05:49:08 -0700

    a

--- dt.js
+++ dt.js
@@ -3,7 +3,7 @@ var data =
     {
         "title":"Script Prank 1",
         "unduh":"https://raw.githubusercontent.com/Gjorka/wahyu.github.io/main/index.html",
-        "date":"Min, 18 Sep"
+        "date":"Sab, 17 Sep"
     },
     {
         "title":"Spam Sms",
