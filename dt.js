d7497b302af57a69af596eb9ff99a932d37d6410
Gjorka <113481480+Gjorka@users.noreply.github.com>
2022-09-18 04:15:21 -0700

    Create dt.js

---
+++ dt.js
@@ -0,0 +1,8 @@
+var data = 
+[
+    {
+        "title":"Script Prank 1",
+        "unduh":"https://raw.githubusercontent.com/Gjorka/wahyu.github.io/main/index.html",
+        "date":"Min, 18 Sep"
+    }
+]
